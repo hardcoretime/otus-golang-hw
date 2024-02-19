ce124d7fa296fcd2e759a2c6d8991f1867e25d62
hardcoretime <36233932+hardcoretime@users.noreply.github.com>
2024-02-19 13:58:36 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,74 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+// isNumInString checks 'str' contains a number.
+func isNumInString(str string) bool {
+	runes := []rune(str)
+
+	for i := 0; i < len(runes)-1; i++ {
+		current := runes[i]
+		next := runes[i+1]
+
+		_, errCurrent := strconv.Atoi(string(current))
+		currentIsDigit := errCurrent == nil
+
+		_, errNext := strconv.Atoi(string(next))
+		nextIsDigit := errNext == nil
+
+		if currentIsDigit && nextIsDigit {
+			return true
+		}
+	}
+
+	return false
+}
+
+func Unpack(packedString string) (string, error) {
+	result := strings.Builder{}
+	runes := []rune(packedString)
+
+	if len(packedString) == 0 {
+		return result.String(), nil
+	}
+
+	if isNumInString(packedString) {
+		return result.String(), ErrInvalidString
+	}
+
+	for i := 0; i < len(runes)-1; i++ {
+		currentRune := runes[i]
+		nextRune := runes[i+1]
+
+		_, errCurrent := strconv.Atoi(string(currentRune))
+		currentRuneIsString := errCurrent != nil
+
+		if i == 0 && !currentRuneIsString {
+			return result.String(), ErrInvalidString
+		}
+
+		nextDigit, errNext := strconv.Atoi(string(nextRune))
+		nextRuneIsDigit := errNext == nil
+
+		if currentRuneIsString && nextRuneIsDigit {
+			result.WriteString(strings.Repeat(string(currentRune), nextDigit))
+		}
+
+		if currentRuneIsString && !nextRuneIsDigit {
+			result.WriteString(string(currentRune))
+		}
+	}
+
+	_, err := strconv.Atoi(string(runes[len(runes)-1]))
+	lastRuneIsString := err != nil
+	if lastRuneIsString {
+		result.WriteString(string(runes[len(runes)-1]))
+	}
+
+	return result.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -16,6 +16,7 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
+		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
 		// uncomment if task with asterisk completed
 		// {input: `qwe\4\5`, expected: `qwe45`},
 		// {input: `qwe\45`, expected: `qwe44444`},
@@ -40,6 +41,29 @@ func TestUnpackInvalidString(t *testing.T) {
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
 			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
+		})
+	}
+}
+
+func TestIsNumInString(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{input: "a4bc2d5e", expected: false},
+		{input: "abc13cd", expected: true},
+		{input: "", expected: false},
+		{input: "aaa0b", expected: false},
+		{input: "d\n5abc", expected: false},
+		{input: "3ab10c", expected: true},
+		{input: "45", expected: true},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.input, func(t *testing.T) {
+			result := isNumInString(tc.input)
+			require.Equal(t, tc.expected, result)
 		})
 	}
 }
